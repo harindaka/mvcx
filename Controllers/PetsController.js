8359256283307741e45bfb584a7fe967e0f393d0
Hemal Harindaka Athulathmudali <harindaka@gmail.com>
2015-11-13 13:16:04 +0530

    removed unnecessary line where an error gets raised

--- Controllers/PetsController.js
+++ Controllers/PetsController.js
@@ -5,7 +5,7 @@ module.exports = function(
 
   this.q = q.value;
 
-  this.get = function(req){throw new Error(test);
+  this.get = function(req){
     return self.view('Index', { pets: [
       { petId: 1, name: 'Good Doge' },
       { petId: 2, name: 'Bad Cate' }
