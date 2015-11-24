344d51c87e68e22385eb8a9a3b937bc901b2636f
Hemal Harindaka Athulathmudali <harindaka@gmail.com>
2015-11-24 16:34:25 +0530

    assets support

--- DefaultConfig.js
+++ DefaultConfig.js
@@ -18,7 +18,7 @@ module.exports = {
     servePath: '/assets', //The virtual path in which assets will be served over HTTP. If hosting assets locally, supply a local path (say, "assets"). If hosting assets remotely on a CDN, supply a URL: "http://myassets.example.com/assets".
     precompile: ["*.*"], //An array of assets to precompile while the server is initializing. Patterns should match the filename only, not including the directory.
     build: false, //Should assets be saved to disk (true) in buildDir, or just served from memory (false)? Set this to false if no write access is allowed for the buildDir
-    buildDir: './assets', //The directory to save (and load) compiled assets to/from.
+    buildDir: './assets/bin', //The directory to save (and load) compiled assets to/from.
     compile: true, //Should assets be compiled if they don’t already exist in the buildDir?
     compress: false, //Should assets be minified? If enabled, requires uglify-js and csswring. Set to true in production
     gzip: false, //Should assets have gzipped copies in buildDir?
